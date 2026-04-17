be52bb6dd83f4bff1965eebf9f9ccea3a6bc78df
Orlando <orlandohabet1993@gmail.com>
2026-04-17 21:31:16 +0800

    wifi connection, dispense now : working dispense counter, switch : coded but not verified

--- water_dispenser_code/water_dispenser_code.ino
+++ water_dispenser_code/water_dispenser_code.ino
@@ -1,16 +1,16 @@
 /* WIFI part */
 #define BLYNK_PRINT Serial
 
-#define BLYNK_TEMPLATE_ID "TMPLaMBC74-X"
-#define BLYNK_TEMPLATE_NAME "NodeMCU"
-#define BLYNK_AUTH_TOKEN "YourAuthToken"
+#define BLYNK_TEMPLATE_ID "TMPL6TvrzGckA"
+#define BLYNK_TEMPLATE_NAME "Chubby Water Dispensor"
+#define BLYNK_AUTH_TOKEN "Q1LbzSd-v4ZbzvICB2O6DJI6WIY3_F_I"
 
 #include <BlynkSimpleEsp8266.h>
 #include <ESP8266WiFi.h>
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char auth[] = "YourAuthToken"; //maybe can get rid of this one
+char auth[] = BLYNK_AUTH_TOKEN; //maybe can get rid of this one
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
@@ -41,8 +41,13 @@ double LIGHT_SWITCH_TIME = 10;
 double light_counter_on = 0;
 double light_counter_off = 0;
 
-bool TEST_MOTOR = 0;
 double DISPENSE_TIME = 2000;
+int dispenses_triggered = 0;
+
+//flags
+bool TEST_MOTOR = 0;
+bool V2_FLAG = 1; // switch (enable/disable function)
+bool V3_FLAG = 0; // dispense now
 
 void setup() {
   // put your setup code here, to run once:
@@ -55,49 +60,60 @@ void setup() {
   Blynk.begin(auth, ssid, pass);
 }
 
+void dispense_water_function() {
+  // dispense water for a set period
+  if(++dispense_time_counter <= DISPENSE_LENGTH)
+  {
+    digitalWrite(pin5, HIGH);  // ON - pump
+  }
+  else
+  {
+    digitalWrite(pin5, LOW);  // OFF - pump
+    dispense_counter = 0;
+    dispense_time_counter = 0;
+    if(V3_FLAG == 1) V3_FLAG = 0; // reset dispense now flag
+    Blynk.virtualWrite(V1, int(++dispenses_triggered));
+  }
+}
+
 void loop() {
   Blynk.run();      //keeps your device connected to the Blynk IoT and processes everything in real time. helps reconnect.
+
+  //light on and off
+  if(++light_counter_on <= LIGHT_SWITCH_TIME)
+  {
+    digitalWrite(pin2, LOW);  // ON - light
+    if(light_counter_on == LIGHT_SWITCH_TIME)
+      light_counter_off = 0;
+  }
+  else if(++light_counter_off <= LIGHT_SWITCH_TIME)
+  {
+    digitalWrite(pin2, HIGH);  // OFF - light
+    if(light_counter_off == LIGHT_SWITCH_TIME)
+      light_counter_on = 0;
+  }
 
   // val=1024-analogRead(yl69_pin);     // reads analogue value like the moisture sensor, then minus 1024 to invert the value to make it easier to understand
   // Serial.println(val);               // Sends the value to Serial Monitor
   //Blynk.virtualWrite(V1, (int)val);   // Sends the value to Blynk IoT app. V1 means virtual pin 1
 
-  // put your main code here, to run repeatedly:
-  if(TEST_MOTOR == 0)
+  if(V3_FLAG == 1)
   {
-    //light on and off
-    if(++light_counter_on <= LIGHT_SWITCH_TIME)
-    {
-      digitalWrite(pin2, LOW);  // ON - light
-      if(light_counter_on == LIGHT_SWITCH_TIME)
-        light_counter_off = 0;
-    }
-    else if(++light_counter_off <= LIGHT_SWITCH_TIME)
-    {
-      digitalWrite(pin2, HIGH);  // OFF - light
-      if(light_counter_off == LIGHT_SWITCH_TIME)
-        light_counter_on = 0;
-    }
+    dispense_water_function();
+  }  
 
+  if(V2_FLAG == 1)
+  {
     // track dispense counter
     if(++dispense_counter >= WHEN_TO_DISPENSE_SECONDS)
     {
-      // dispense water for a set period
-      if(++dispense_time_counter <= DISPENSE_LENGTH)
-      {
-        digitalWrite(pin5, HIGH);  // ON - pump
-      }
-      else
-      {
-        digitalWrite(pin5, LOW);  // OFF - pump
-        dispense_counter = 0;
-        dispense_time_counter = 0;
-      }
+      //function to dispense water
+      dispense_water_function();
     }
-
-    delay(100); // loop once every 0.1 second
   }
-  else
+  
+  // test motor
+  if (TEST_MOTOR == 1)
   {
     // Turn OFF pump
     digitalWrite(pin2, HIGH);  // OFF - light
@@ -109,4 +125,16 @@ void loop() {
     digitalWrite(pin5, HIGH);  // ON - pump
     delay(DISPENSE_TIME);      // Run time
   }
+  delay(100); // loop once every 0.1 second
+}
+
+// enable / disable function
+BLYNK_WRITE(V2) {
+  bool value = param.asInt();
+  V2_FLAG = value;
+}
+
+// dispense now
+BLYNK_WRITE(V3) {
+    V3_FLAG = 1;
 }
